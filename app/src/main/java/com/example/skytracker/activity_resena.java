5bd723f7d4f2f7cc0ec232be0f1d172feec0f9c8
Jacobsc0 <j.solano3@alumnos.santotomas.cl>
2024-09-24 17:23:59 -0300

    Prototipo Final

--- app/src/main/java/com/example/skytracker/activity_resena.java
+++ app/src/main/java/com/example/skytracker/activity_resena.java
@@ -1,24 +1,48 @@
 package com.example.skytracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class activity_resena extends AppCompatActivity {
+
+    private Spinner motivoSpinner;
+    private RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_resena);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        motivoSpinner = findViewById(R.id.spinner);
+        ratingBar = findViewById(R.id.ratingBar);
+
+
+        Button btnRegresar = findViewById(R.id.btnRegresar);
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(activity_resena.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(activity_resena.this, "Gracias por contactarnos", Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
